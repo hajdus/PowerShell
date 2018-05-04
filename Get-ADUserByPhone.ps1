f61e44fa70af585b232b140a3828d89b6fd8e238
Michal Hajduk <hajduk.mich@gmail.com>
2018-05-04 16:38:08 +0200

    Update Get-ADUserByPhone.ps1

--- Get-ADUserByPhone.ps1
+++ Get-ADUserByPhone.ps1
@@ -15,30 +15,30 @@ autor: hajdus 2018
 
 
 Param(
-  [Parameter(Mandatory=$true)] [String]$Phone_number
+    [Parameter(Mandatory = $true)] [String]$Phone_number
 )
 
 #Making 3 substrings from entered phone number        
-$Part1=$Phone_number.Substring(0,3)
-$Part2=$Phone_number.Substring(3,3)
-$Part3=$Phone_number.Substring(6,3)
+$Part1 = $Phone_number.Substring(0, 3)
+$Part2 = $Phone_number.Substring(3, 3)
+$Part3 = $Phone_number.Substring(6, 3)
 
 
 $phone0 = $Phone_number
-$phone1= "+48"+$Part1+$Part2+$Part3
-$phone2= "+48"+" "+$Part1+$Part2+$Part3
-$phone3= "+48"+" "+$Part1+" "+$Part2+" "+$Part3
-$phone4= $Part1+" "+$Part2+" "+$Part3
-$phone5= $Part1+"-"+$Part2+"-"+$Part3
+$phone1 = "+48" + $Part1 + $Part2 + $Part3
+$phone2 = "+48" + " " + $Part1 + $Part2 + $Part3
+$phone3 = "+48" + " " + $Part1 + " " + $Part2 + " " + $Part3
+$phone4 = $Part1 + " " + $Part2 + " " + $Part3
+$phone5 = $Part1 + "-" + $Part2 + "-" + $Part3
 
 
-for ($i=0; $i -le 5; $i++ ){
+for ($i = 0; $i -le 5; $i++ ) {
       
     $phonetxt = Get-Variable -Name "phone$i" -ValueOnly
     echo "Looking for account for number: $phonetxt" 
     
     Get-ADUser -Filter * -Properties name, MobilePhone, OfficePhone |
-    Where-Object {$_.MobilePhone -eq (Get-Variable -Name "phone$i" -ValueOnly) -or $_.OfficePhone -eq (Get-Variable -Name "phone$i" -ValueOnly)} 
+        Where-Object {$_.MobilePhone -eq (Get-Variable -Name "phone$i" -ValueOnly) -or $_.OfficePhone -eq (Get-Variable -Name "phone$i" -ValueOnly)} 
 }
 
 
--- Get-GooglePlayAppUpdateDate.ps1
+++ Get-GooglePlayAppUpdateDate.ps1
@@ -12,49 +12,48 @@ autor: hajdus 2018
 
 
 
-Function Get-AppUpdateDate{
+Function Get-AppUpdateDate {
 
-Param(
-  [Parameter(Mandatory=$true)] [String]$url
-)
+    Param(
+        [Parameter(Mandatory = $true)] [String]$url
+    )
 
-$Response = Invoke-WebRequest $url
+    $Response = Invoke-WebRequest $url
 
-#Get app name from google play store
-$AppName = $Response.AllElements | Where id -eq "main-title" | Select -First 1 -ExpandProperty innerText   
-$AppName = $AppName.Split()
-$AppName = $AppName[0]
+    #Get app name from google play store
+    $AppName = $Response.AllElements | Where id -eq "main-title" | Select -First 1 -ExpandProperty innerText   
+    $AppName = $AppName.Split()
+    $AppName = $AppName[0]
 
-#Get app update date form google play store
-$UpdateDate = $Response.AllElements | Where Class -eq "htlgb" | Select -First 1 -ExpandProperty innerText    
+    #Get app update date form google play store
+    $UpdateDate = $Response.AllElements | Where Class -eq "htlgb" | Select -First 1 -ExpandProperty innerText    
 
-#Copy previous new data to old.txt file
-Copy-Item -Path "$AppName new.txt" -Destination "$AppName old.txt"
+    #Copy previous new data to old.txt file
+    Copy-Item -Path "$AppName new.txt" -Destination "$AppName old.txt"
 
-#Save present data to new.txt file
-$UpdateDate | Out-File -FilePath "$AppName new.txt"
+    #Save present data to new.txt file
+    $UpdateDate | Out-File -FilePath "$AppName new.txt"
 
-#Compare old and new data from txt files
-If( Compare-Object -ReferenceObject $(Get-Content "$AppName old.txt") -DifferenceObject $(Get-Content "$AppName new.txt"))
-#Find app update and sand email
- {  
+    #Compare old and new data from txt files
+    If ( Compare-Object -ReferenceObject $(Get-Content "$AppName old.txt") -DifferenceObject $(Get-Content "$AppName new.txt"))
+    #Find app update and sand email {  
     $MyEmail = "ExampleMail@gmail.com"
     $Password = 'PASSWORD' 
-    $SMTP= "smtp.gmail.com"
+    $SMTP = "smtp.gmail.com"
     $To = "ExampleMail@gmail.com"
     $Subject = "$AppName new update"
     $Body = "$AppName has been updated! Last version is from $UpdateDate. Available at the $url"
     
     $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force  
-    $Creds = New-Object "System.Management.Automation.PSCredential" -ArgumentList $MyEmail,$SecurePassword
+    $Creds = New-Object "System.Management.Automation.PSCredential" -ArgumentList $MyEmail, $SecurePassword
 
     Send-MailMessage -To $To -From $MyEmail -Subject $Subject -Body $Body -SmtpServer $SMTP -Credential $Creds -UseSsl -Port 587 -DeliveryNotificationOption never
-  }
+}
 
 #No app update
 Else {}
  
- }
+}
 
 #Calling function
 Get-AppUpdateDate "https://play.google.com/store/apps/details?id=com.whatsapp"
