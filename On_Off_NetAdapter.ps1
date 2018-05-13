26f0a564e1dd7b44e5a48826606c8bb854a67464
Michal Hajduk <hajduk.mich@gmail.com>
2018-05-13 17:39:58 +0200

    Update On_Off_NetAdapter.ps1

--- On_Off_NetAdapter.ps1
+++ On_Off_NetAdapter.ps1
@@ -9,14 +9,12 @@ autor: hajdus 2018
 
 
 $eth = Get-NetAdapter -name "Ethernet" 
-if ($eth.Status -Match "Up")
-{
-Disable-NetAdapter -name "Ethernet" -Confirm:$false
-Exit
+if ($eth.Status -Match "Up") {
+    Disable-NetAdapter -name "Ethernet" -Confirm:$false
+    Exit
 }
 
-if ($eth.Status -Match "Disabled")
-{
-Enable-NetAdapter -name "Ethernet" -Confirm:$false
-Exit
+if ($eth.Status -Match "Disabled") {
+    Enable-NetAdapter -name "Ethernet" -Confirm:$false
+    Exit
 }
