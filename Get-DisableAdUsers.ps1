98b5eb251a86fc79ad6fd75d051f3c822f4afaa6
hajdus <32811798+hajdus@users.noreply.github.com>
2018-05-16 15:37:43 +0200

    Create Get-DisableAdUsers.ps1

---
+++ Get-DisableAdUsers.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+Get all disable AD users
+.DESCRIPTION
+Create txt file on curent user desktop which conteins all disable users and last log-on date
+.EXAMPLE
+Get-DisableAdUsers.ps1 
+.NOTES
+autor: hajdus 2018
+#>
+
+$desktop = [Environment]::GetFolderPath("Desktop")
+
+get-aduser -Filter * -SearchBase "DC=DC,DC=corp"  -properties name, LastLogonDate, Enabled |
+Where-Object {$_.Enabled -like "False"} |
+Select name, LastLogonDate |
+out-file $desktop\DisableUsers.txt -fo -en UTF8 
+
