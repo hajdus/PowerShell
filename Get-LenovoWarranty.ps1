9a533375566194e0b97afee663c72efb5a976604
Michal Hajduk <hajduk.mich@gmail.com>
2018-06-03 20:11:54 +0200

    Update Get-LenovoWarranty.ps1

--- Get-LenovoWarranty.ps1
+++ Get-LenovoWarranty.ps1
@@ -1,27 +1,24 @@
 <#
 .SYNOPSIS
-
+    Get lenovo computer warranty from csv file 
 .DESCRIPTION
-
+    For all serialnumber form csv file create html output with Product Name, Serial Number,
+    Warranty End Date. If computer dont have warranty then it will be in red backgroud in output file.
 .EXAMPLE
     Get-LenovoWarranty.ps1
 .NOTES
-autor: hajdus 2018
+    autor: hajdus 2018
 #>
 
 
+$FilePath = "C:\temp"
+$csv = Import-Csv "$FilePath\lenovowarranty.csv" -Delimiter ";" 
+$date = Get-Date -UFormat "%Y-%m-%d"
 
-$csv = Import-Csv C:\Users\mhajduk1\Desktop\lenovowarranty.csv -Delimiter ";" 
-$FilePath = "C:\Users\mhajduk1\Desktop\"
-$date = Get-Date -Format g
-
-if (Test-Path "$FilePath\Lenovo.html" -PathType Leaf){
-Remove-Item "$FilePath\Lenovo.html" }
-
-
-
- 
-
+#Check if output file exist
+if (Test-Path "$FilePath\Lenovo.html" -PathType Leaf) {
+    Remove-Item "$FilePath\Lenovo.html" 
+}
 
 
 ConvertTo-Html -Body "
@@ -33,19 +30,17 @@ Generated: $date
 <TD width=250px ><font face=verdana color=Black><H3>Warranty End Date</H3></font></TD>
 </TR>
 </table>" -Title "Lenovo Warranty" | 
-Out-File -Append "$FilePath\Lenovo.html" 
+    Out-File -Append "$FilePath\Lenovo.html" 
 
+$csv | ForEach-Object {
 
+    #Dont need to define model
+    #$Model = $_.Model
 
-$csv |ForEach-Object{
+    #Get sn form csv file
+    $SerialNumber = $_.SerialNumber
 
-#Dont need to define model
-#$Model = $_.Model
-
-#Get sn form csv file
-$SerialNumber = $_.SerialNumber
-
-$body =
+    $body =
 @"
 xml=<wiInputForm source='ibase'>
 <id>LSC3</id>
@@ -58,29 +53,36 @@ xml=<wiInputForm source='ibase'>
 </wiInputForm>
 "@
 
-   $searchResults = Invoke-RestMethod -Method Post https://ibase.lenovo.com/POIRequest.aspx -ContentType application/x-www-form-urlencoded -Body $body
-        
-    
-   
-   [xml]$xml = $searchResults 
-   
-   #Product Name
-$ProductName = $xml.wiOutputForm.warrantyInfo.machineinfo.productName 
+    $searchResults = Invoke-RestMethod -Method Post https://ibase.lenovo.com/POIRequest.aspx -ContentType application/x-www-form-urlencoded -Body $body
 
+    [xml]$xml = $searchResults 
+    #Product Name
+    $ProductName = $xml.wiOutputForm.warrantyInfo.machineinfo.productName 
     #Serial number
-$SerialNumber = $xml.wiOutputForm.warrantyInfo.machineinfo.serial  
-
+    $SerialNumber = $xml.wiOutputForm.warrantyInfo.machineinfo.serial  
     #Warranty end date
-$WED = $xml.wiOutputForm.warrantyInfo.serviceinfo.wed
+    $WED = $xml.wiOutputForm.warrantyInfo.serviceinfo.wed
 
- 
-ConvertTo-Html -Body "<Table border=1>
-<TR>
-<TD width=250px >$ProductName</TD>
-<TD width=250px >$SerialNumber</TD>
-<TD width=250px >$WED</TD>
-</TR>
-</table>" | Out-File -Append "$FilePath\Lenovo.html" 
+    #if current date is lower then warranty end date then color row green
+    if ($date -lt $WED[0]) {
+        ConvertTo-Html -Body "<Table border=1>
+        <TR>
+        <TD width=250px  bgcolor=green>$ProductName</TD>
+        <TD width=250px  bgcolor=green>$SerialNumber</TD>
+        <TD width=250px bgcolor=green>$WED</TD>
+        </TR>
+        </table>" | Out-File -Append "$FilePath\Lenovo.html"
+    }
 
-   
-   }
+    #if current date is grater then warranty end date then color row red
+    else {
+        ConvertTo-Html -Body "<Table border=1>
+    <TR>
+    <TD width=250px bgcolor=red>$ProductName</TD>
+    <TD width=250px bgcolor=red>$SerialNumber</TD>
+    <TD width=250px bgcolor=red>$WED</TD>
+    </TR>
+    </table>" | Out-File -Append "$FilePath\Lenovo.html" 
+    }
+
+}
