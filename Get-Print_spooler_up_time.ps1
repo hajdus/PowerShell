7b495627b1a2ee6e3475a71134fbea38d1b49359
Michal Hajduk <hajduk.mich@gmail.com>
2019-06-27 20:40:56 +0200

    Change

--- Get-Print_spooler_up_time.ps1
+++ Get-Print_spooler_up_time.ps1
@@ -1,17 +1,5 @@
-<#
-.SYNOPSIS
-    Get print spooler up time
-.DESCRIPTION
-  Get print spooler up time
-.EXAMPLE
-    Get-Print_spooler_up_time.ps1  
-.NOTES
-    autor: hajdus 2018
-#>
 
+$computer = "10.110.12.80"
 
-
-$computer = "localhost"
-
-Enter-PSSession -ComputerName $computer -Credential username
-(Get-EventLog  -LogName â€œSystemâ€ -Source â€œService Control Managerâ€ -EntryType â€œInformationâ€ -Message â€œ*Print Spooler service*running*â€ -Newest 1).TimeGenerated
+Enter-PSSession -ComputerName $computer -Credential mh
+(Get-EventLog  -LogName “System” -Source “Service Control Manager” -EntryType “Information” -Message “*Print Spooler service*running*” -Newest 1).TimeGenerated
