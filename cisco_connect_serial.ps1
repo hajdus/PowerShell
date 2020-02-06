e28abf693d6d38a895e47630c8fc7ca48ceeaf8d
hajdus <32811798+hajdus@users.noreply.github.com>
2020-02-06 12:27:00 +0100

    Add files via upload

---
+++ cisco_connect_serial.ps1
@@ -0,0 +1,25 @@
+﻿date
+
+ $port= new-Object System.IO.Ports.SerialPort COM6,9600,None,8,one
+ $port.Open()
+ $port.WriteLine(“ en”)
+ $port.WriteLine(“ terminal length 0”)
+ $port.WriteLine(“ sh run”)
+
+ $message = ""
+ 
+ while ($message -notlike "end*") {   
+  $message=$port.ReadLine()  
+  Write-Output $message 
+  $port.WriteLine("") 
+}  
+ 
+ 
+ 
+ 
+ $port.Close()
+
+
+ date
+
+
