954f139def8dccef62411757d176f6e45045bba7
hajdus <32811798+hajdus@users.noreply.github.com>
2020-02-06 09:01:57 +0100

    Add files via upload

---
+++ AutoUpdate_FsrmFileGroup.ps1
@@ -0,0 +1,23 @@
+﻿
+Function ConvertFrom-Json20
+{
+    # Deserializes JSON input into PowerShell object output
+    Param (
+        [Object] $obj
+    )
+    Add-Type -AssemblyName System.Web.Extensions
+    $serializer = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+    return ,$serializer.DeserializeObject($obj)
+}
+
+
+$jsonStr = Invoke-WebRequest -Uri https://fsrm.experiant.ca/api/v1/get
+$monitoredExtensions = @(ConvertFrom-Json20 $jsonStr | ForEach-Object { $_.filters })
+
+
+
+Set-FsrmFileGroup -Name "Ransomware Files" -IncludePattern $monitoredExtensions
+
+
+#(Get-FsrmFileGroup -Name "Ransomware Files").IncludePattern.count
+
